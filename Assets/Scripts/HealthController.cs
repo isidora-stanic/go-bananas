885a9bbfc9d7d1c14d014af152797d4848b07a78
isidora-stanic <isidora.stanic@outlook.com>
2023-05-20 16:02:07 +0200

    docs: added docs to HealthController; refactor: added headers, changed public fields into serializefield private fields, added public property IsDead

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -2,46 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>HealthController</c> manages health, healing, taking damage and diying of a character.
+/// </summary>
 public class HealthController : MonoBehaviour
 {
-    public float maxHealth = 100f;
-    public float health = 100f;
-    public float healAmount = 1f;
-    public float healSpeed = 5f;
-    [SerializeField] private bool mainCharacter;
-
     
-    private bool isDead = false;
+    enum TypeOfCharacter {Main, Enemy, Side};
 
+    [Header("Health and Healing properties")]
+    [SerializeField] private float maximumHealth = 100f;
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float healAmount = 1f;
+    [SerializeField] private float healSpeed = 5f;
+
+    [Header("Special indicators")]
+    [SerializeField] private TypeOfCharacter characterType;
+    [SerializeField] private bool indestructable;
+    private bool isDead = false;
+    public bool IsDead { get { return isDead; } } // another class can only get this value
+
+
+    /// <summary>
+    /// Method <c>TakeDamage</c> decreases health by given damage.
+    /// Checks if character has no more health and calls Die method if so.
+    /// If a character is indestructuble, it doesn't do anything.
+    /// </summary>
     public void TakeDamage(float damage)
     {
+        if (indestructable) return;
         health -= damage;
         if (health <= 0f)
         {
             Die();
         }
-        if (mainCharacter)
+        if (characterType == TypeOfCharacter.Main)
             Debug.Log("Main character took damage, current health: " + health);
-        else Debug.Log("Enemy took damage, current health: " + health);
+        else if (characterType == TypeOfCharacter.Enemy)
+            Debug.Log("Enemy took damage, current health: " + health);
+        else Debug.Log("Side character took damage, current health: " + health);
     }
 
-    // razmisli da li treba da se u posebnoj niti proverava da li je lik crko
+    /// <summary>
+    /// Method <c>Die</c> sets indicator isDead to true and destroys game object of a character.
+    /// </summary>
     private void Die()
     {
         isDead = true;
-        if (mainCharacter)
+        if (characterType == TypeOfCharacter.Main)
             Debug.Log("Main character just died.");
-        else Debug.Log("Enemy just died.");
+        else if (characterType == TypeOfCharacter.Enemy)
+            Debug.Log("Enemy just died.");
+        else Debug.Log("Side character just died.");
 
-        Destroy(gameObject);
+        Destroy(gameObject); // TODO: consider only seting active to false for performance reasosns
         // some kind of restart
     }
 
+    /// <summary>
+    /// Method <c>Heal</c> heals a character if needed, for heal amount by heal speed.
+    /// </summary>
     private IEnumerator Heal()
     {
         while (!isDead)
         {
-            if (health < maxHealth)
+            if (health < maximumHealth)
             {
                 health += healAmount;
                 // Debug.Log("Healing. Current health: " + health);
@@ -50,6 +75,9 @@ public class HealthController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Method <c>Start</c> starts a coroutine for healing.
+    /// </summary>
     void Start()
     {
         StartCoroutine(Heal());
